6577e1e530706aaef232a3104f053422c0537615
Ankit Kumar <ankit.kumarrrrr6055@gmail.com>
2025-10-27 00:14:32 +0530

    Navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import ExploreBtn from "./ExploreBtn";
+import ExploreBtn from "../components/ExploreBtn";
 
 const page = () => {
   return (
@@ -10,6 +10,16 @@ const page = () => {
         Hackathons, Meetups, and Conferences, All in one Place
       </p>
       <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {[1,2, 3, 4, 5].map((event) => (
+            <li key={event}>Event {event}</li>
+          ))}
+        </ul>
+      </div>
+
     </section>
   );
 };
